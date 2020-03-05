718e0dbb7225c2366eddc3570645079b89d8b00a
Aleksei Shashin <aleksei.shashin@gmail.com>
2020-03-05 23:51:37 +0100

    Added popup dialog

--- airport-app/web/src/App.js
+++ airport-app/web/src/App.js
@@ -26,6 +26,9 @@ class App extends React.Component {
     var featuresData = cooridantes.map(function(coordinate) {
       var formattedPoint = {
         type: "Feature",
+        properties: {
+          airportDetails: "TESTT"
+        },
         geometry: {
           type: "Point",
           coordinates: [coordinate[0], coordinate[1]]
@@ -95,6 +98,40 @@ class App extends React.Component {
       console.log("Call on load function");
     });
 
+    // Create a popup, but don't add it to the map yet.
+    var popup = new mapboxgl.Popup({
+      closeButton: false,
+      closeOnClick: false
+    });
+
+    // Change the cursor to a pointer when the mouse is over the places layer.
+    map.on("mouseenter", "airport-layer", function(e) {
+      map.getCanvas().style.cursor = "pointer";
+
+      var coordinates = e.features[0].geometry.coordinates.slice();
+      var description = e.features[0].properties.description;
+
+      // Ensure that if the map is zoomed out such that multiple
+      // copies of the feature are visible, the popup appears
+      // over the copy being pointed to.
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+
+      // Populate the popup and set its coordinates
+      // based on the feature found.
+      popup
+        .setLngLat(coordinates)
+        .setHTML(description)
+        .addTo(map);
+    });
+
+    // Change it back to a pointer when it leaves.
+    map.on("mouseleave", "airport-layer", function() {
+      map.getCanvas().style.cursor = "";
+      popup.remove();
+    });
+
     //TODO another way to set map as global variable
     this.globalMap = map;
   }
@@ -137,6 +174,13 @@ class App extends React.Component {
           var cooridantes = response.data.map(function(p) {
             return [p.longitude, p.latitude];
           });
+
+          // var values = response.data.map(function(p) {
+          //   return {
+          //     cooridantes: [p.longitude, p.latitude],
+          //     second: "det"
+          //   };
+          // });
 
           this.drawAirportPoints(cooridantes);
 
